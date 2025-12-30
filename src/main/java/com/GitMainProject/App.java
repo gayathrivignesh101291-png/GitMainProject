5ae79e033c0659e2127f0c4f1ec6c019a49fcd7c
gayathrivignesh101291-png <gayathrivignesh101291@gmail.com>
2025-12-30 15:48:56 +0530

    Update App.java

--- src/main/java/com/GitMainProject/App.java
+++ src/main/java/com/GitMainProject/App.java
@@ -1,17 +1,5 @@
 package com.GitMainProject;
 
 public class App {
-
-    add();
-    
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("100 %");
-        
-        
-        System.out.println(" hi all")
-        System.out.println("dsfssdgdrg");
-        
-        
-    }
+    System.out.println("hi");
 }
