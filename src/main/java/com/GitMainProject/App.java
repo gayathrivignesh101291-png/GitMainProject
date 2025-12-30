f6d4c9036456ae75d5daeebc1f4bd1481847d176
gayathri <gayathrisundaram91@gmail.com>
2025-12-30 12:50:41 +0530

    updated

---
+++ src/main/java/com/GitMainProject/App.java
@@ -0,0 +1,13 @@
+package com.GitMainProject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("100 %");
+        
+        System.out.println("gadfhfgj");
+    }
+}
